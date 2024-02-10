edece36a7f144be6487e01c74f8081f04e57d5f1
David Atlan <david@davipro.fr>
2024-02-10 17:18:45 +0100

    Added employee creation functionality to the REST API.

--- api/src/main/java/fr/davipro/api/controller/EmployeeController.java
+++ api/src/main/java/fr/davipro/api/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.davipro.api.model.Employee;
@@ -17,13 +19,14 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     /**
-    * Read - Get all employees
-    * @return - An Iterable object of Employee full filled
-    */
-    @GetMapping("/employees")
-    public Iterable<Employee> getEmployees() {
-        return employeeService.getEmployees();
-    }
+	 * Create - Add a new employee
+	 * @param employee An object employee
+	 * @return The employee object saved
+	 */
+	@PostMapping("/employee")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeService.saveEmployee(employee);
+	}
 
     /**
 	 * Read - Get one employee 
@@ -38,7 +41,15 @@ public class EmployeeController {
 		} else {
 			return null;
 		}
-	}
-    
+    }
+
+    /**
+    * Read - Get all employees
+    * @return - An Iterable object of Employee full filled
+    */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
 	
 }
