775212c02455fb04658072b0ef6e504394350943
David Atlan <david@davipro.fr>
2024-02-10 17:09:51 +0100

    Ajout du EndPoint "/employees" à l'api REST.

---
+++ api/src/main/java/fr/davipro/api/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package fr.davipro.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.davipro.api.model.Employee;
+import fr.davipro.api.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+    * Read - Get all employees
+    * @return - An Iterable object of Employee full filled
+    */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+}
