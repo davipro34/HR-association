cfbe7cb60e34b2cc0230097f8ab944911198a836
David Atlan <david@davipro.fr>
2024-02-10 16:27:03 +0100

    First commit

---
+++ api/src/test/java/fr/davipro/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.davipro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
